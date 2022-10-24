8423903a6803acd6e029069dcc78ff6b9c98dab1
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-24 21:40:26 +0100

    Update DataStaxAstraProperties.java

--- src/main/java/com/valleon/inboxapp/DataStaxAstraProperties.java
+++ src/main/java/com/valleon/inboxapp/DataStaxAstraProperties.java
@@ -1,19 +1,22 @@
 package com.valleon.inboxapp;
 
+
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 
+
+@Configuration
 @Getter
 @Setter
-@Configuration
 @ConfigurationProperties(prefix = "datastax.astra")
 public class DataStaxAstraProperties {
 
+    
     private File secureConnectBundle;
-
 
 }
