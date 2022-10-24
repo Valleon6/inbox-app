bd14a72e8f39b9a413a2456c26e21ccb835597b3
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-24 14:58:52 +0100

    Create FolderRepository.java

---
+++ src/main/java/com/valleon/inboxapp/folders/FolderRepository.java
@@ -0,0 +1,9 @@
+package com.valleon.inboxapp.folders;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends CassandraRepository<Folder, String> {
+
+}
