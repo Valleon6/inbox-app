a507ba56c1a6879423d9ffa5a6ffd809975f3fab
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-25 22:03:20 +0100

    Update InboxController.java

--- src/main/java/com/valleon/inboxapp/controllers/InboxController.java
+++ src/main/java/com/valleon/inboxapp/controllers/InboxController.java
@@ -1,5 +1,6 @@
 package com.valleon.inboxapp.controllers;
 
+import com.valleon.inboxapp.emailList.EmailListItemRepository;
 import com.valleon.inboxapp.folders.Folder;
 import com.valleon.inboxapp.folders.FolderRepository;
 import com.valleon.inboxapp.folders.FolderService;
@@ -18,24 +19,27 @@ public class InboxController {
 
     @Autowired
     private FolderRepository folderRepository;
-
     @Autowired
     private FolderService folderService;
+    @Autowired
+    private EmailListItemRepository emailListItemRepository;
 
-    @GetMapping(value =  "/")
-    private String homePage(@AuthenticationPrincipal OAuth2User principal, Model model){
+    @GetMapping(value = "/")
+    private String homePage(@AuthenticationPrincipal OAuth2User principal, Model model) {
 
-        if(principal == null || StringUtils.hasText(principal.getAttribute("login"))){
+        if (principal == null || StringUtils.hasText(principal.getAttribute("login"))) {
             return "index";
         }
         {
             String userId = principal.getAttribute("login");
 
-           List<Folder> userFolders =  folderRepository.findAllById(userId);
-           model.addAttribute("userFolders", userFolders);
-
-            List<Folder> defaultFolders =  folderService.fetchDefaultFolder(userId);
+            //Fetch folders
+            List<Folder> userFolders = folderRepository.findAllById(userId);
+            model.addAttribute("userFolders", userFolders);
+            List<Folder> defaultFolders = folderService.fetchDefaultFolder(userId);
             model.addAttribute("defaultFolders", defaultFolders);
+
+            //Fetch messages
 
             return "inbox-app";
         }
